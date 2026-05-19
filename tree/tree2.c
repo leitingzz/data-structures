f09e3dcc23160cafc21fae1d3024768a64e8efe5
leitingzz <3479309040@qq.com>
2026-05-19 22:05:59 +0800

    更新

---
+++ tree/tree2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Treenode
+{
+    int data;
+    struct Treenode *childL;
+    struct Treenode *childR;
+};
+
+struct Treenode  *insert(struct Treenode *root,int data)
+{
+    if (root == NULL)
+    {
+        struct Treenode *node = (struct Treenode *)malloc(sizeof(struct Treenode));
+        node->data = data;
+        node->childL = NULL;
+        node->childR = NULL;
+
+        return node;
+    }
+
+    if (data < root->data)
+    {
+        root->childL = insert(root->childL,data);
+    }
+
+    else if (data > root->data)
+    {
+        root->childR = insert(root->childR,data);
+    }
+
+    return root;
+};
+
+void inOrder(struct Treenode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    else
+    {
+        inOrder(root->childL);
+        printf("%d\n",root->data);
+        inOrder(root->childR);
+    }
+}
+
+void FreeTree(struct Treenode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    FreeTree(root->childL);
+    FreeTree(root->childR);
+
+    printf("正在释放：%d\n",root->data);
+    free(root);
+}
+
+int main()
+{
+    struct Treenode *root = NULL;
+    int num[] = {10,5,15,3,7};
+    for (int i = 0;i < 5;i++)
+    {
+        root = insert(root,num[i]);
+    }
+    
+    inOrder(root);
+
+    FreeTree(root);
+
+    return 0;
+}
