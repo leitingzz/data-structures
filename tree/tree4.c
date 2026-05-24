baf826abfa1d4bbaaa5da4a7bd3c879e20e144db
leitingzz <3479309040@qq.com>
2026-05-24 23:46:01 +0800

    更新

--- tree/tree4.c
+++ tree/tree4.c
@@ -7,3 +7,38 @@ struct treeNode{
     struct treeNode* left;
     struct treeNode* right;
 };
+
+int getHeight(struct treeNode* n){
+    return (n == NULL) ? 0 : n->height;
+}
+
+int getBalance(struct treeNode* n){
+    return (n == NULL) ? 0 : getHeight(n->left) - getHeight(n->right);
+}
+
+void updataHeight(struct treeNode* n){
+    int leftH = getHeight(n->left);
+    int rightH = getHeight(n->right);
+    n->height = ((leftH > rightH) ? leftH : rightH) + 1;
+}
+
+struct treeNode* insert(struct treeNode* root,int data){
+    if (root == NULL){
+        root = (struct treeNode*)malloc(sizeof(struct treeNode));
+        root->data = data;
+        root->height = 1;
+        root->left = root->right = NULL;
+    }
+
+    if (data > root->data){
+        root->right = insert(root->right,data);
+    }else if (data < root->data){
+        root->left = insert(root->left);
+    }else return root;
+
+    updataHeight(root);
+
+    int balance = getBalance(root);
+
+    
+}
