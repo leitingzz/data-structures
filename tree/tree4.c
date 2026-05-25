61e2804cb0eb7a184faf4f68f310e5293536757d
leitingzz <3479309040@qq.com>
2026-05-25 23:56:16 +0800

    更新

--- tree/tree4.c
+++ tree/tree4.c
@@ -22,6 +22,22 @@ void updataHeight(struct treeNode* n){
     n->height = ((leftH > rightH) ? leftH : rightH) + 1;
 }
 
+struct treeNode* leftRotate(struct treeNode* root){
+    struct treeNode* temp1 = root->right->left;
+    struct treeNode* temp2 = root->right;
+    root->right->left = root;
+    root->right = temp1;
+    return temp2;
+}
+
+struct treeNode* rightRotate(struct treeNode* root){
+    struct treeNode* temp1 = root->left;
+    struct treeNode* temp2= root->left->right;
+    root->left->right = root;
+    root->left = temp2;
+    return temp1;
+}
+
 struct treeNode* insert(struct treeNode* root,int data){
     if (root == NULL){
         root = (struct treeNode*)malloc(sizeof(struct treeNode));
@@ -33,12 +49,24 @@ struct treeNode* insert(struct treeNode* root,int data){
     if (data > root->data){
         root->right = insert(root->right,data);
     }else if (data < root->data){
-        root->left = insert(root->left);
+        root->left = insert(root->left,data);
     }else return root;
 
     updataHeight(root);
 
     int balance = getBalance(root);
 
+    if (balance > 1 && data < root->left->data) return rightRotate(root);
+    if (balance < -1 && data > root->right->data) return leftRotate(root);
     
+    if (balance > 1 && data > root->left->data){
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+    if (balance < -1 && data < root->right->data){
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
 }
