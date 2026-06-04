57c6c8bf315a6a714abf3c602bc9856b03e9eae1
leitingzz <3479309040@qq.com>
2026-06-04 14:02:25 +0800

    更新

--- graph.c/graph3.c
+++ graph.c/graph3.c
@@ -4,10 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_VERTICES 10
+#define MAX_VERTICES 4
 
 //链表节点结构
-typedef struct{
+typedef struct Node{
     int vertex;
     Node* next;
 }Node,*myNode;
@@ -36,5 +36,35 @@ void addEdge(myGraph g, int src, int dest){
     g->adjList[dest] = newNode;
 }
 
+void initGraph(myGraph g, int vertices){
+    g->numVertex = vertices;
+    for(int i = 0; i < vertices; i++){
+        g->adjList[i] = NULL;
+    }
+}
 
+void printGraph(myGraph g){
+    for(int i = 0; i < g->numVertex; i++){
+        myNode temp = g->adjList[i];
+        while (temp != NULL)
+        {
+            printf("%d->",temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
 
+int main(){
+    myGraph g;
+    initGraph(g, 4);
+
+    addEdge(g, 0, 1);
+    addEdge(g, 0, 2);
+    addEdge(g, 1, 3);
+    addEdge(g, 2, 3);
+
+    printGraph(g);
+
+    return 0;
+}
