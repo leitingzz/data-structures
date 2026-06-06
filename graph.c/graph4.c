1d0069a181c1636704c0979d595fc91de2484bbd
leitingzz <3479309040@qq.com>
2026-06-06 19:38:51 +0800

    更新

--- graph.c/graph4.c
+++ graph.c/graph4.c
@@ -16,3 +16,25 @@ typedef struct{
     myNode adjList[MAX_VERTICES];
 }graph,*mygraph;
 
+myNode creatNode(int n){
+    myNode newNode = (myNode)malloc(sizeof(Node));
+    newNode->vertex = n;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void initgraph(mygraph g){
+    for (int i = 0; i < g->numVertices; i++){
+            g->adjList[i] = NULL;
+    }
+}
+
+void addEdge(mygraph g, int src, int dest){
+    myNode temp = creatNode(dest);
+    temp->next = g->adjList[src];
+    g->adjList[src] = temp;
+
+    myNode temp = creatNode(src);
+    temp->next = g->adjList[dest];
+    g->adjList[dest] = temp;
+}
